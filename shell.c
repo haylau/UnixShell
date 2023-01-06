b931c194e1bb8778a1d233d75523033dce43dde6
Danny <danny.n.kha@gmail.com>
2023-01-06 15:51:44 -0800

    New functions for redirect out and in

--- shell.c
+++ shell.c
@@ -4,10 +4,27 @@
 
 #define MAX_LINE 80 /* The maximum length command */
 
+// function to redirect out to file
+void redirectOut(char *fileName) 
+{
+    int outFile = open(fileName, O_WRONLY | O_CREAT); // opening file to output
+    dup2(outFile, STDOUT_FILENO); // duplicating stdout with outFile
+    close(outFile); // close the outFile
+}
+
+// function to redirect in to file
+void redirectIn(char *fileName)
+{
+    int inFile = open(fileName, O_RDONLY | O_CREAT); // opening file to read input
+    dup2(inFile, STDIN_FILENO); // duplicating stdin with inFile
+    close(inFile); // close the inFile
+}
+
+// main function
 int main(void)
 {
-  char **args[MAX_LINE/2 + 1]; /* command line arguments */
-  char **history[MAX_LINE/2 +1]; // command line history
+  char *args[MAX_LINE/2 + 1]; /* command line arguments */
+  char *history[MAX_LINE/2 +1]; // command line history
   int should_run = 1; /* flag to determine when to exit program */
 
   while (should_run) {
@@ -48,27 +65,19 @@ int main(void)
                 printf("No commands in history. \n");
             }
             
-            int j = 0;
-            while(args[j] != NULL)
+            int i = 0;
+            while(args[i] != NULL)
             {
                 // redirect operators
-                if (strcmp(args[j], ">") == 0)
+                if (strcmp(args[i], ">") == 0) // redirecting out
                 {
-                    int outFile = open(args[j+1], O_WRONLY | O_CREAT);
-                    dup2(outFile, STDOUT_FILENO);
-                    close(outFile);
-                    args[j] = NULL;
-                    args[j+1] = NULL;
+                    redirectOut(args[i+1]); // sending token after '>' to redirect out function
                 }
-                else if (stcmp(args[j], "<") == 0)
+                else if (stcmp(args[i], "<") == 0) // redirecting in
                 {
-                    int inFile = open(args[j+1], O_WRONLY | O_CREAT);
-                    dup2(inFile, STDIN_FILENO);
-                    close(inFile);
-                    args[j] = NULL;
-                    args[j+1] = NULL;
+                    redirectIn(args[i+1]); // sending token after '<' to redirect in function
                 }
-                ++j;
+                ++i;
             }
         
             execvp(args[0], args); // invoking execvp
@@ -91,3 +100,4 @@ int main(void)
   }
   return 0;
 }
+
