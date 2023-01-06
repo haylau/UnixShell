18ed367dea769eca7f1b96d002eb9fa8907b3530
Hayden Lauritzen <haydenlauritzen@gmail.com>
2023-01-06 11:45:13 -0800

    init

---
+++ shell.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define MAX_LINE 80 /* The maximum length command */
+
+int main(void)
+{
+  char **args[MAX_LINE/2 + 1]; /* command line arguments */
+  int should_run = 1; /* flag to determine when to exit program */
+
+  while (should_run) {
+    printf("osh>");
+    fflush(stdout);
+
+    // fetch user cmd
+    char* input[MAX_LINE];
+    size_t len = MAX_LINE;
+    ssize_t lineSize = 0;
+    lineSize = getline(input, &len, stdin);
+    if (len > 0) {
+        (*input)[len - 1] = '\0';
+    }
+
+    // tokenize user cmd
+    char* token = strtok(input, ' ');
+    int i = 0;
+    *args[i] = token;
+    while(token != NULL) {
+        // grab token
+        token = strtok(token, ' ');
+        ++i;    
+        *args[i] = token;
+    }
+    
+    int pid = fork();
+    switch(pid) {
+        case -1 : {
+            // fork failed
+            break;
+        }
+        case 0 : {
+            // new fork
+            break;
+        }
+        default : {
+            // old fork
+            break;
+        }
+    }
+
+    /**
+    * After reading user input, the steps are:
+    * (1) fork a child process using fork()
+    * (2) the child process will invoke execvp()
+    * (3) parent will invoke wait() unless command included &
+    */
+
+  }
+  return 0;
+}
