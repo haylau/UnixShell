10fc0a806384d9458c86371fdfb4cac8f49ea865
Hayden Lauritzen <haydenlauritzen@gmail.com>
2023-01-07 01:37:51 -0800

    push

--- shell.c
+++ shell.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <string.h>
 
 #define MAX_LINE 80 /* The maximum length command */
+
+// function to redirect out to file
+void redirectOut(char* fileName)
+{
+    int outFile = open(fileName, O_WRONLY | O_CREAT); // opening file to output
+    dup2(outFile, STDOUT_FILENO); // duplicating stdout with outFile
+    close(outFile); // close the outFile
+}
+
+// function to redirect in to file
+void redirectIn(char* fileName)
+{
+    int inFile = open(fileName, O_RDONLY | O_CREAT); // opening file to read input
+    dup2(inFile, STDIN_FILENO); // duplicating stdin with inFile
+    close(inFile); // close the inFile
+}
 
 void createChildProc(char** args, char cmdTerm) {
     pid_t pid = fork();
@@ -42,28 +59,12 @@ void createChildProc(char** args, char cmdTerm) {
     }
     default: {
         // parent fork
-        if (strcmp(cmdTerm, ';') == 0) {
-            WAIT(NULL);
+        if (cmdTerm == ';') {
+            wait(NULL);
         }
-        break; 
+        break;
     }
     }
-}
-
-// function to redirect out to file
-void redirectOut(char* fileName)
-{
-    int outFile = open(fileName, O_WRONLY | O_CREAT); // opening file to output
-    dup2(outFile, STDOUT_FILENO); // duplicating stdout with outFile
-    close(outFile); // close the outFile
-}
-
-// function to redirect in to file
-void redirectIn(char* fileName)
-{
-    int inFile = open(fileName, O_RDONLY | O_CREAT); // opening file to read input
-    dup2(inFile, STDIN_FILENO); // duplicating stdin with inFile
-    close(inFile); // close the inFile
 }
 
 // main function
@@ -82,42 +83,48 @@ int main(void)
         size_t len = MAX_LINE;
         ssize_t lineSize = 0;
         lineSize = getline(input, &len, stdin);
-        if (len > 0) {
-            (*input)[len - 1] = '\0';
+        if (lineSize > 0) {
+            (*input)[lineSize - 1] = '\0';
         }
 
         // tokenize user cmd
-        char* token = strtok(input, " ");
+        const char delim[2] = " ";
         int i = 0;
+        char* token = strtok(*input, delim);
         args[i] = token;
         while (token != NULL) {
             // grab token
-            token = strtok(token, " ");
+            token = strtok(NULL, delim);
             ++i;
             args[i] = token;
         }
 
         // fork for every command found
-        char* cmd;
+        char* cmd[MAX_LINE / 2 + 1];
         i = 0;
+        int j = 0;
         while (args[i] != NULL) {
             // found end of command
-            if (strcmp(args[i], ";") || strcmp(args[i], "&")) {
-                createChildProc(cmd, args[i]);
+            if (strcmp(args[i], ";") == 0 || strcmp(args[i], "&") == 0) {
+                cmd[j] = NULL;
+                createChildProc(cmd, args[i][0]);
                 // empty cmd
-                cmd = NULL;
+                cmd[0] = NULL;
+                j = 0;
             }
             // found pipe operator
-            else if (strcmp(args[i], "|")) {
+            else if (strcmp(args[i], "|") == 0) {
                 // #TODO pipe?? 
             }
             // else found an arguement
             else {
-                cmd[i] = args[i];
+                cmd[j] = args[i];
             }
             ++i;
+            ++j;
         }
         // end of input
+        cmd[j] = NULL;
         createChildProc(cmd, ';');
 
         /**
